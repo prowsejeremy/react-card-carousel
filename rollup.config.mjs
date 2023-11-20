c96c2d9c9c1e4bdb7e8cc7626c7645c3f6171552
Jeremy Prowse <jeremyprowse@Jeremys-MacBook-Pro.local>
2023-11-20 18:02:14 +1300

    try new build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,28 @@
-import { babel } from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 import css from 'rollup-plugin-import-css';
 
-export default [
-  {
-    input: 'src/index.js',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-      },
-      {
-        file: 'dist/index.es.js',
-        format: 'es',
-        exports: 'named',
-      }
-    ],
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react']
-      }),
-      css(),
-      commonjs(),
-      resolve(),
-      terser()
-    ]
-  }
-];
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'dist/index.js', format: 'cjs' },
+    { file: 'dist/index.es.js', format: 'esm' }
+  ],
+  plugins: [
+    nodeResolve({
+      extensions: ['.js', '.jsx']
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-react'],
+      extensions: ['.js', '.jsx']
+    }),
+    css(),
+    commonjs(),
+    replace({
+      preventAssignment: false
+    })
+  ]
+}
